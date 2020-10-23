52bda46172a4aa0a7624cc4e9fa75b20223dbc5e
ivanps-ped <61473474+ivanps-ped@users.noreply.github.com>
2020-10-23 19:41:29 +0200

    MovimentoBasico

---
+++ Assets/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    private int speed = 20;
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){
+
+            m_Rigidbody.velocity += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_Rigidbody.velocity += -transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_Rigidbody.velocity += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_Rigidbody.velocity += -transform.right * speed * Time.deltaTime;
+        }
+    }
+}
